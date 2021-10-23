e9cf263708a51cce43c2662816137ec6c912af77
Randy Kwalar <rkwalar@gmail.com>
2021-10-23 02:06:10 +0100

    Add colors for project

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+// COLORS
+const Color? primaryColor = Color.fromRGBO(0, 0, 0, 1);
+const Color? accentColor = Color.fromRGBO(0, 87, 251, 1);
+const Color? whiteColor = Color.fromRGBO(255, 255, 255, 1);
+const Color? lightWhiteColor = Color.fromRGBO(255, 255, 255, 0.1);
+const Color? desaturatedGreyColor = Color.fromRGBO(229, 229, 229, 1);
+const Color? veryLightGreyColor = Color.fromRGBO(220, 220, 220, 1);
+const Color? lightGreyColor = Color.fromRGBO(155, 155, 155, 1);
+const Color? greyColor = Color.fromRGBO(120, 120, 120, 1);
+const Color? orangeColor = Color.fromRGBO(255, 119, 74, 1);
+const Color? lightOrangeColor = Color.fromRGBO(255, 119, 74, 0.1);
+const Color? greenColor = Color.fromRGBO(61, 209, 121, 1);
+const Color? lightGreenColor = Color.fromRGBO(61, 209, 121, 0.1);
+const Color? redColor = Color.fromRGBO(241, 50, 62, 1);
+const Color? lightRedColor = Color.fromRGBO(241, 50, 62, 0.1);
+const Color? purpleColor = Color.fromRGBO(210, 80, 231, 1);
+const Color? lightPurpleColor = Color.fromRGBO(210, 80, 231, 0.1);
+
+// TEXT STYLES
+const TextStyle h1TextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 30,
+);
+
+const TextStyle h6TextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+);
+
+const TextStyle subtitleBoldTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
+
+const TextStyle bodyTextStyle = const TextStyle(
+  fontWeight: FontWeight.normal,
+  fontSize: 16,
+);
+
+const TextStyle bodyBoldTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+);
+
+const TextStyle captionTextStyle = const TextStyle(
+  fontWeight: FontWeight.normal,
+  fontSize: 14,
+);
+
+const TextStyle captionBoldTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+);
+
+const TextStyle labelTextStyle = const TextStyle(
+  fontWeight: FontWeight.normal,
+  fontSize: 12,
+);
+
+const TextStyle labelBoldTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+);

7853eda8832bf2f870c4c5e232d199e94522fe99
Randy Kwalar <rkwalar@gmail.com>
2021-10-23 02:19:55 +0100

    Deleted unnecessary font file

--- lib/screens/basic.dart
+++ lib/screens/basic.dart
@@ -1,4 +1,5 @@
 import 'package:basic/constants/color_constants.dart';
+import 'package:basic/constants/textstyle_constants.dart';
 import 'package:flutter/material.dart';
 
 import 'package:basic/screens/splash/splash_screen.dart';

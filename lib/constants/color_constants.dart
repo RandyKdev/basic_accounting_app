499ad91fdeba9d287b24f0a0a0a3a658ec2b8842
Randy Kwalar <rkwalar@gmail.com>
2021-10-23 02:17:25 +0100

    Changed font weight of text styles

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -20,46 +20,46 @@ const Color? lightPurpleColor = Color.fromRGBO(210, 80, 231, 0.1);
 
 // TEXT STYLES
 const TextStyle h1TextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
   fontSize: 30,
 );
 
 const TextStyle h6TextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
   fontSize: 20,
 );
 
 const TextStyle subtitleBoldTextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
   fontSize: 18,
 );
 
 const TextStyle bodyTextStyle = const TextStyle(
-  fontWeight: FontWeight.normal,
+  fontWeight: FontWeight.w400,
   fontSize: 16,
 );
 
 const TextStyle bodyBoldTextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
   fontSize: 16,
 );
 
 const TextStyle captionTextStyle = const TextStyle(
-  fontWeight: FontWeight.normal,
+  fontWeight: FontWeight.w400,
   fontSize: 14,
 );
 
 const TextStyle captionBoldTextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
   fontSize: 14,
 );
 
 const TextStyle labelTextStyle = const TextStyle(
-  fontWeight: FontWeight.normal,
+  fontWeight: FontWeight.w400,
   fontSize: 12,
 );
 
 const TextStyle labelBoldTextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w700,
   fontSize: 12,
 );

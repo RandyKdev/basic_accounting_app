64109f51e26aacef30619268a1cde2d51dfa8070
Randy Kwalar <rkwalar@gmail.com>
2021-10-31 19:26:37 +0100

    Add new product screen

--- lib/basic.dart
+++ lib/basic.dart
@@ -9,6 +9,7 @@ import 'package:basic/screens/auth/setup_account_screen.dart';
 import 'package:basic/screens/auth/verify_number_screen.dart';
 import 'package:basic/screens/dashboard/dashboard_screen.dart';
 import 'package:basic/screens/main/main_screen.dart';
+import 'package:basic/screens/new/new_product_screen.dart';
 import 'package:basic/screens/sales/sales_screen.dart';
 import 'package:basic/screens/upload/upload_product_screen.dart';
 import 'package:basic/screens/upload/upload_service_screen.dart';
@@ -54,7 +55,7 @@ class Basic extends StatelessWidget {
         primaryColor: primaryColor,
         scaffoldBackgroundColor: desaturatedGreyColor,
       ),
-      home: UploadProductScreen(),
+      home: NewProductScreen(),
       debugShowCheckedModeBanner: false,
     );
   }

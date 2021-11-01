1b055215d3b33b441db78cc44ef50d045cfbfe21
Randy Kwalar <rkwalar@gmail.com>
2021-11-01 15:46:16 +0100

    Add spacing class

---
+++ lib/classes/spacing_class.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class SpacingClass {
+  static const SizedBox hrVerySmall = SizedBox(width: 1);
+  static const SizedBox hrSmall = SizedBox(width: 5);
+  static const SizedBox hrMedium = SizedBox(width: 10);
+  static const SizedBox hrRegular = SizedBox(width: 15);
+  static const SizedBox hrBig = SizedBox(width: 20);
+  static const SizedBox hrVeryBig = SizedBox(width: 40);
+
+  static const SizedBox vrVerySmall = SizedBox(height: 1);
+  static const SizedBox vrSmall = SizedBox(height: 5);
+  static const SizedBox vrMedium = SizedBox(height: 10);
+  static const SizedBox vrRegular = SizedBox(height: 15);
+  static const SizedBox vrBig = SizedBox(height: 20);
+  static const SizedBox vrVeryBig = SizedBox(height: 40);
+}
---
+++ lib/widgets/appbar_button_widget.dart
@@ -0,0 +1,53 @@
+import 'package:basic/Enums/button_size_enums.dart';
+import 'package:basic/classes/gap_class.dart';
+import 'package:basic/constants/color_constants.dart';
+import 'package:basic/constants/textstyle_constants.dart';
+import 'package:flutter/material.dart';
+
+class AppBarButtonWidget extends StatelessWidget {
+  final void Function() onTap;
+  final ButtonSize size;
+  final Widget icon;
+  final String text;
+  final bool isIconSameSize;
+  const AppBarButtonWidget({
+    required this.icon,
+    required this.isIconSameSize,
+    required this.onTap,
+    required this.size,
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            vertical: SizeUnitsClass.regular,
+            horizontal: size == ButtonSize.small
+                ? SizeUnitsClass.regular
+                : SizeUnitsClass.big,
+          ),
+          decoration: BoxDecoration(
+              color: lightWhiteColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(SizeUnitsClass.regular),
+              )),
+          child: Row(
+            children: [
+              icon,
+              SizedBox(width: 15),
+              Text(
+                text,
+                style: labelBoldTextStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
